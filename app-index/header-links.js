4dc0209bc7e3355833386554fe9b9dcede69b89c
MalickGalant <89748688+MalickGalant@users.noreply.github.com>
2022-12-12 17:35:07 +0200

    Create header-links.js

---
+++ app-index/header-links.js
@@ -0,0 +1,15 @@
+module.exports = [
+  {
+    'name': 'Github',
+    'href': 'https://github.com/MalickGalant/problog/',
+  },
+  {
+    'name': 'CircleCI',
+    'href':
+      'https://malickgalant.github.io/problog?branch=main',
+  },
+  {
+    'name': 'Percy',
+    'href': 'https://percy.io/maickgalant/problog/',
+  },
+];
